420d8c99f8e2f317ee43605dda6119d8805dcb18
zitongliu <zitongliu7@gmail.com>
2018-04-18 02:12:00 +1000

    created add gift tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {gifts: []};
+  }
+  addGift = () => {
+    const { gifts } = this.state;
+    
+    const ids = this.state.gifts.map(gift => gift.id);
+    
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+    gifts.push({ id: max_id+1});
+    
+    this.setState({ gifts });
+  }
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button onClick={this.addGift} className='btn-add'>Add Gift</Button>
       </div>
     );
   }
