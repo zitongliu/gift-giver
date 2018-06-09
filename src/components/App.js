8c248e97c6b093d1745683d44b8673d910a9d0cc
zitongliu <zitongliu7@gmail.com>
2018-06-10 09:17:00 +1000

    created add gift unit test

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,32 @@ import { Button } from 'react-bootstrap';
 class App extends Component {
   constructor() {
     super();
-    
+
     this.state = {gifts: []};
   }
   addGift = () => {
     const { gifts } = this.state;
-    
+
     const ids = this.state.gifts.map(gift => gift.id);
-    
+
     const max_id = ids.length > 0 ? Math.max(...ids) : 0;
     gifts.push({ id: max_id+1});
-    
+
     this.setState({ gifts });
   }
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <div className='gift-list'>
+          {
+            this.state.gifts.map(gift => {
+              return (
+                <div key={gift.id}></div>
+              )
+            })
+          }
+        </div>
         <Button onClick={this.addGift} className='btn-add'>Add Gift</Button>
       </div>
     );
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -15,6 +15,12 @@ it('initializes the `state` with an empty list of gifts', () => {
 
 it('adds a new gift to the `state` when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click');
-  
+
   expect(app.state().gifts).toEqual([{id: 1}]);
+});
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.find('.gift-list').children().length).toEqual(2);
 });
