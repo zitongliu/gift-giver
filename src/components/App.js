ce867d3f049c7a38584371b98db96e29840060f6
zitongliu <zitongliu7@gmail.com>
2018-06-10 14:05:11 +1000

    added coverage options and max_number helper util

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper';
 
 class App extends Component {
   constructor() {
@@ -10,10 +11,8 @@ class App extends Component {
   }
   addGift = () => {
     const { gifts } = this.state;
-
     const ids = this.state.gifts.map(gift => gift.id);
-
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+    const max_id = max_number(ids);
     gifts.push({ id: max_id+1});
 
     this.setState({ gifts });
