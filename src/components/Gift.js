0f3ccd13e67e60d22f941eea3cce4ff9cc3b73d6
zitongliu <zitongliu7@gmail.com>
2018-06-10 10:26:31 +1000

    created gift component and tests

---
+++ src/components/Gift.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class Gift extends Component {
+  constructor() {
+    super();
+
+    this.state = { person: '', present: '' };
+  }
+  render() {
+    return (
+      <div>
+        <Form>
+          <FormGroup>
+            <ControlLabel>Person</ControlLabel>
+            <FormControl
+              className='input-person'
+              onChange={event => this.setState({ person: event.target.value })}
+            />
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Gift;
